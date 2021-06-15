21c9a3ac02a675150906f1ffcf89a99ade542064
SAKSHAM <sakshamratra.0106@gmail.com>
2021-06-15 23:30:09 +0530

    Local Watcher Created

--- FileWatcher.py
+++ FileWatcher.py
@@ -10,8 +10,8 @@ from Fetcher import *
 from local_file_watcher import *
 
 
-copy_file_from_loc_dir(src_loc, source_filename, dst_loc, target_filename)
-copy_file_from_s3(bucket_name, source_filename, dst_loc, target_filename)
-copy_file_from_sftp(sftp_remote_path, sftp_source_filename, dst_loc, target_filename)
+# copy_file_from_loc_dir(src_loc, source_filename, dst_loc, target_filename)
+# copy_file_from_s3(bucket_name, source_filename, dst_loc, target_filename)
+# copy_file_from_sftp(sftp_remote_path, sftp_source_filename, dst_loc, target_filename)
 
-watch_local_file(src_loc + forward_slash + source_filename, "file", 1, 10)
+watch_local_file(src_loc + forward_slash + source_filename, dst_loc + forward_slash + target_filename, "file", 1, 10)
--- local_file_watcher.py
+++ local_file_watcher.py
@@ -2,7 +2,6 @@
 
 import os
 import time
-import sys
 import datetime
 from Fetcher import copy_file_from_loc_dir
 
@@ -13,12 +12,20 @@ def files_to_timestamp(path):
 
 
 def file_to_timestamp(src_file_loc):
-    return dict([(src_file_loc, os.path.getmtime(src_file_loc))])
+    try:
+        final_dictionary = dict([(src_file_loc, os.path.getmtime(src_file_loc))])
+    except WindowsError:
+        final_dictionary = dict()
+    except:
+        final_dictionary = dict()
+
+    return final_dictionary
 
 
-def watch_local_file(path_to_watch, source_type_folder_or_file,hours_to_watch = 0 ,sleep_time = 2):
+def watch_local_file(path_to_watch, destination_location, source_type_folder_or_file, hours_to_watch = 0 ,sleep_time = 2):
 
-    print('Watching {source_type_folder_or_file} {path_to_watch}'.format(path_to_watch))
+    print('Watching {source_type} {watch}'.format(source_type= source_type_folder_or_file,
+                                                  watch= path_to_watch))
 
     current_date_and_time = datetime.datetime.now()
     print(current_date_and_time)
@@ -26,9 +33,13 @@ def watch_local_file(path_to_watch, source_type_folder_or_file,hours_to_watch = 
     end_date_and_time = current_date_and_time + hours_added
     print(end_date_and_time)
 
-    if source_type_folder_or_file=="file" : before = file_to_timestamp(path_to_watch)
-    elif source_type_folder_or_file=="folder" : before = files_to_timestamp(path_to_watch)
-    else : sys.exit('source_type_folder_or_file can only be either file or folder type')
+    before = file_to_timestamp(path_to_watch)
+
+    # Below Commented code is to include folder watch
+
+    # if source_type_folder_or_file=="file" : before = file_to_timestamp(path_to_watch)
+    # elif source_type_folder_or_file=="folder" : before = files_to_timestamp(path_to_watch)
+    # else : sys.exit('source_type_folder_or_file can only be either file or folder type')
 
     while datetime.datetime.now() < end_date_and_time:
         time.sleep(sleep_time)
@@ -43,8 +54,27 @@ def watch_local_file(path_to_watch, source_type_folder_or_file,hours_to_watch = 
                 if os.path.getmtime(f) != before.get(f):
                     modified.append(f)
 
-        if added: print('Added: {}'.format(', '.join(added)))
+        if added :
+            print('Added: {}'.format(', '.join(added)))
+            print('Moving File: {}'.format(', '.join(added)))
+            filename = path_to_watch.split("\\")[-1]
+            filepath = path_to_watch.replace("\\" + path_to_watch.split("\\")[-1], "")
+
+            destination_filename = destination_location.split("\\")[-1]
+            destination_filepath = destination_location.replace("\\" + destination_location.split("\\")[-1], "")
+            copy_file_from_loc_dir(filepath, filename, destination_filepath, destination_filename)
+
         if removed: print('Removed: {}'.format(', '.join(removed)))
-        if modified: print('Modified: {}'.format(', '.join(modified)))
+
+        if modified:
+            print('modified: {}'.format(', '.join(modified)))
+            print('Moving File: {}'.format(', '.join(modified)))
+            filename = path_to_watch.split("\\")[-1]
+            filepath = path_to_watch.replace("\\" + path_to_watch.split("\\")[-1], "")
+
+            destination_filename = destination_location.split("\\")[-1]
+            destination_filepath = destination_location.replace("\\" + destination_location.split("\\")[-1], "")
+            copy_file_from_loc_dir(filepath, filename, destination_filepath, destination_filename)
+
         print(datetime.datetime.now())
         before = after
